243da79647383bc0ed79693f2e91fc0df72af7a7
MrUse77 <agusdor14@hotmail.com>
2023-01-14 13:51:51 -0300

    angular 0.1

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { BordeComponent } from './components/borde/borde.component';
+import { AboutComponent } from './components/about/about.component';
+import { InfoComponent } from './components/info/info.component';
+import { SkillComponent } from './components/skill/skill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    BordeComponent,
+    AboutComponent,
+    InfoComponent,
+    SkillComponent
   ],
   imports: [
     BrowserModule,
---
+++ Portfolio/src/app/components/borde/borde.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-borde',
+  templateUrl: './borde.component.html',
+  styleUrls: ['./borde.component.css']
+})
+export class BordeComponent {
+
+}
