7c76642e16f1b5a10df66705fd665c866aa924db
MrUse77 <agusdor14@hotmail.com>
2023-05-08 16:35:30 -0300

    Ver 1.0

--- Portfolio/src/app/components/educacion/educacion.component.ts
+++ Portfolio/src/app/components/educacion/educacion.component.ts
@@ -12,40 +12,6 @@ import { DatosService } from 'src/app/service/datos.service';
   templateUrl: './educacion.component.html',
   styleUrls: ['./educacion.component.scss']
 })
-<<<<<<< HEAD
-export class EducacionComponent {
-  secu= ["EEST N°8", "TECNICO EN ELECTRONICA", "2016-2022"];
-  curso = [
-    "UTN HAEDO", "CURSO DE REPARACION DE PC","Abril 2019 - Septiembre 2019",
-    "ARGENTINA PROGRAMA", "1° ETAPA #SeProgramar", "junio 2022","","2° ETAPA #YoProgramo", "CURSO DE DESARROLLO WEB FULL STACK","En curso"
-          ];
-  show=0;
-  show1=0;
-  show2=0;
-<<<<<<< HEAD
-  show3=0;
-=======
-  show3=0
->>>>>>> 0f1035e79c3aaef1443053f11f9a60218c258dd6
-  @ViewChild('info1')info1!: ElementRef;
-  @ViewChild('info2')info2!: ElementRef; 
-  @ViewChild('info3')info3!: ElementRef; 
-  @ViewChild('info4')info4!: ElementRef; 
-  @ViewChild('imagen') imagen!:ElementRef;
-  @ViewChild('imagen2') imagen2!:ElementRef;
-  @ViewChild('imagen3') imagen3!:ElementRef;
-  @ViewChild('imagen4') imagen4!:ElementRef;
-  @ViewChild('agregar') agregar!:ElementRef;
-<<<<<<< HEAD
-  @ViewChild('edit') edit!:ElementRef;
-  @ViewChild('editar') editar!:ElementRef;
-  @ViewChild('edicion') edicion!:ElementRef;
-=======
->>>>>>> 0f1035e79c3aaef1443053f11f9a60218c258dd6
-
-  constructor(private render2: Renderer2,
-    ){
-=======
 export class EducacionComponent implements OnInit{
   edu:  Educacion[] = [];
   edu2:Educacion[] = [];
@@ -92,7 +58,6 @@ export class EducacionComponent implements OnInit{
       }),
       }
     )
->>>>>>> develop
   }
   ObtenerDatos(){
     this.service.DatosEducacion().subscribe(data => {
@@ -126,78 +91,7 @@ export class EducacionComponent implements OnInit{
       this.render2.setStyle(newEdu2, 'display', 'none');
       this.render2.setStyle(signup, 'display', 'none');
     }
-<<<<<<< HEAD
-    if(num == 'utn'){
-      this.render2.setStyle(info2,'opacity','1');
-      this.render2.setStyle(info2,'top','107px');
-      this.render2.setStyle(info2,'visibility','visible');
-      this.render2.setStyle(imagen2,'visibility','hidden');
-      this.render2.setStyle(imagen2,'opacity','0');
-      this.render2.setStyle(imagen2,'top','70%');
-      this.show1 ++;
-      if(this.show1 == 2){
-        this.render2.setStyle(info2, 'opacity', '0');
-        this.render2.setStyle(info2,'top','60%');
-        this.render2.setStyle(info2, 'visibility', 'hidden');
-        this.render2.setStyle(imagen2,'top','150px');
-        this.render2.setStyle(imagen2,'opacity','1');
-        this.render2.setStyle(imagen2,'visibility','visible');
-        this.show1 = 0;
-      }
-    }
-    if(num == 'arg_pro'){
-      this.render2.setStyle(info3,'opacity','1');
-      this.render2.setStyle(info3,'visibility','visible');
-      this.render2.setStyle(info3,'top','107px');
-      this.render2.setStyle(imagen3,'visibility','hidden');
-      this.render2.setStyle(imagen3,'opacity','0');
-      this.render2.setStyle(imagen3,'top','70%');
-      this.show2 ++;
-      if(this.show2 == 2){
-        this.render2.setStyle(info3, 'opacity', '0');
-        this.render2.setStyle(info3, 'visibility', 'hidden');
-        this.render2.setStyle(info3,'top','60%');
-        this.render2.setStyle(imagen3,'opacity','1');
-        this.render2.setStyle(imagen3,'visibility','visible');
-        this.render2.setStyle(imagen3,'top','150px');
-        this.show2 = 0;
-      }
-    }
-    if(num == 'arg_pro2'){
-      this.render2.setStyle(info4,'opacity','1');
-      this.render2.setStyle(info4,'visibility','visible');
-      this.render2.setStyle(info4,'top','70px');
-      this.render2.setStyle(imagen4,'visibility','hidden');
-      this.render2.setStyle(imagen4,'opacity','0');
-      this.render2.setStyle(imagen4,'top','70%');
-      this.show3 ++;
-      if(this.show3 == 2){
-        this.render2.setStyle(info4, 'opacity', '0');
-        this.render2.setStyle(info4, 'visibility', 'hidden');
-        this.render2.setStyle(info4,'top','60%');
-        this.render2.setStyle(imagen4,'opacity','1');
-        this.render2.setStyle(imagen4,'visibility','visible');
-        this.render2.setStyle(imagen4,'top','150px');
-        this.show3 = 0;
-      }
-    }
 }
-<<<<<<< HEAD
-SaveInfo(){
-  const edit = this.edit.nativeElement;
-  const editar = this.editar.nativeElement;
-  const edicion = this.edicion.nativeElement;
-  this.render2.setStyle(edit, 'display', 'none');
-  this.render2.setStyle(editar, 'display', 'block');
-  this.render2.setStyle(edicion, 'display', 'none');
-}
-EditInfo(){
-  const edit = this.edit.nativeElement;
-  const editar = this.editar.nativeElement;
-  const edicion = this.edicion.nativeElement;
-=======
-  }
->>>>>>> develop
 
 NewEdu(form: Educacion){
   this.service.CrearEducacion(form).subscribe((data2)=>{
@@ -223,9 +117,5 @@ cerrarVentana(){
   await this.service.EditarEducacion(id, form2);
   this.ObtenerDatos();
 }
-<<<<<<< HEAD
-=======
->>>>>>> 0f1035e79c3aaef1443053f11f9a60218c258dd6
-=======
->>>>>>> develop
+
 }
